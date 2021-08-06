21b73b1cefa5915a8e1ad313034dba590bfb57a7
AtomicNicos <atomicnicos@gmail.com>
2021-08-06 18:50:23 +0200

    Push in 10 minutes

---
+++ the-jspanse/f0.js
@@ -0,0 +1,36 @@
+const m = {};
+const input = document.getElementById('input');
+const answer = document.getElementById('answer');
+
+const txt = 'Have fun!<br/>~ <a href="https://twitter.com/AtomicNicos">AtomicNicos</a>@ASV-DC29';
+answer.innerHTML = txt;
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    connectParticles: true,
+    color: ['#20c20e', '#0c8900', '#39ff13'],
+    speed: 0.3,
+    sizeVariations: 4,
+    maxParticles: 150
+  });
+};
+input.addEventListener('input', e =>{
+  const handler = o => {
+    if (o.status) {
+      answer.classList.add('green');
+      answer.innerHTML = `Good job! You got the flag!`;
+    } else {
+      answer.classList.add('red');
+      answer.innerText = `Nope!`;
+      
+      setTimeout(()=>{
+        answer.classList = '';
+        answer.innerHTML = txt;
+      }, 500);
+    }
+  }
+  
+  if (!m[0](e.target.value, handler)) {
+    handler({status: false});
+  }
+});
